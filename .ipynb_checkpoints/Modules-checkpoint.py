156fcfea3ca3396582d7fb19b478ee06125693b1
khickey25 <khickey25@users.noreply.github.com>
2019-04-15 12:24:46 -0400

    working on baseline classifier for sequential data

--- .ipynb_checkpoints/Modules-checkpoint.py
+++ .ipynb_checkpoints/Modules-checkpoint.py
@@ -80,7 +80,6 @@ def evaluate_baseline(df, NB = True):
     from sklearn.naive_bayes import GaussianNB
     from sklearn.linear_model import LogisticRegression
     from sklearn.model_selection import train_test_split
-    from sklearn.naive_bayes import GaussianNB
     from sklearn.linear_model import LogisticRegression
     from sklearn.model_selection import train_test_split, KFold, cross_val_score
     from sklearn.metrics import classification_report, confusion_matrix
